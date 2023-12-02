81128d913972627e13b3d976e1ecafb7911b4a98
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2023-12-01 19:28:46 -0700

    building-simplel-liker

--- main.js
+++ main.js
@@ -2,7 +2,34 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const errorModal = document.getElementById("modal")
+const errorMessage = document.getElementById("modal-message")
+errorModal.classList.add('hidden')
+
+const similarElements = document.querySelectorAll(".like-glyph");
+
+similarElements.forEach(element => {
+  element.addEventListener('click', (e) => {
+    mimicServerCall()
+    .then(() => {
+      console.log(e.target)
+      if (e.target.classList.contains('activated-heart')){
+        e.target.classList.remove('activated-heart')
+        e.target.textContent = EMPTY_HEART
+      } else {
+        e.target.classList.add("activated-heart")
+        e.target.textContent = FULL_HEART
+        
+    }
+    })
+    .catch((error) =>{
+      console.log(error)
+      errorModal.classList.remove('hidden')
+      errorMessage.textContent = error;
+      setTimeout(() => {errorModal.classList.add('hidden')}, 3000)
+    })
+  })
+})
 
 
 
